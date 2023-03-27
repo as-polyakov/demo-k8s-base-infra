58fa2d984356ce3d853c2d9000cfb00256fc8433
Anton Polyakov <polyakov.anton@gmail.com>
2023-03-26 21:19:46 -0700

    sensitive

--- main.tf
+++ main.tf
@@ -1,39 +1,41 @@
 terraform {  
-backend "s3" {  
-      
-  }  
+    backend "s3" {  
+
+    }  
 }
 
 variable "cluster_name" {type=string}
 module eks {
     source = "./modules/eks"
-    cluster_name = var.cluster_name
+        cluster_name = var.cluster_name
 }
 
 output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+    description = "Endpoint for EKS control plane"
+        value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+    description = "Security group ids attached to the cluster control plane"
+        value       = module.eks.cluster_security_group_id
 }
 
 output "region" {
-  description = "AWS region"
-  value       = var.region
+    description = "AWS region"
+        value       = var.region
 }
 
 output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+    description = "Kubernetes Cluster Name"
+        value       = module.eks.cluster_name
 }
 
 output "cluster_token" {
     value = module.eks.cluster_token
+        sensitive = true
 }
 output "cluster_ca_certificate" {
     value = module.eks.cluster_ca_certificate
+        sensitive = true
 }
 
